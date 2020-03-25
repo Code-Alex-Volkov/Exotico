de6cfc7b52affeb7491863497e15616067ad3c32
Alex_Volkov <alexvolkov72305@yandex.ru>
2020-03-25 22:38:44 +0300

    22:40 25.03.2020

--- src/assets/scripts/modules/jqueryScript.js
+++ src/assets/scripts/modules/jqueryScript.js
@@ -8,11 +8,36 @@ $(document).ready(function(){
 	'use strict';
    /*alert('Приветик');*/
 
-   $('.home_slider').slick({
-   	vertical: true,
-   	arrows: false,
-   	dots: true
-   });
+	/*home_slider*/
+	if ($('.home_slider').length) {
+		$('.home_slider').slick({
+	   	vertical: true,
+	   	arrows: false,
+	   	dots: true
+	   });
+	}
+
+   /*Popup Video*/
+   if ($('.open_video').length) {
+   	$('.open_video').magnificPopup({
+		  type: 'iframe'
+		});
+   }
+   
+
+	/*Carusel Slider for Shop Items*/
+	if ($('.shop_carusel').length) {
+		$('.shop_carusel').slick({
+		  infinite: true,
+		  slidesToShow: 4,
+		  slidesToScroll: 1,
+		  appendArrows: $('.shop_slider_navigation'),
+		  prevArrow: "<div class=\"left\"><i class=\"fas fa-arrow-left\"></i></div>",
+		  nextArrow: "<div class=\"right\"><i class=\"fas fa-arrow-right\"></i></div>"
+		});
+	}
+
+	
 
 });
 
